5ace870601d8c6ef2173ed57ba6479125c161376
Shafiat Siddiquee Arafat <shafiat825@gmail.com>
2025-12-29 15:07:00 -0500

    attempt

--- cypress/e2e/Day02_121625/Brave_search_loop.cy.js
+++ cypress/e2e/Day02_121625/Brave_search_loop.cy.js
@@ -6,7 +6,7 @@ describe("Brave search functionality with loop", () => {
     keywords.push("JavaScript")
     keywords.push("TyoeScript")
     keywords.push("Cypress Automation Framework")
-    
+
 
     it("Search url and verify the url contains", () => {
         cy.visit("https://search.brave.com/");
@@ -14,4 +14,4 @@ describe("Brave search functionality with loop", () => {
         cy.url().should("contain", "Cypress+Automation");
     } )
 
-})
+})// End of describe Suite
